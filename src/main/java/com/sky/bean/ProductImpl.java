7220385de970c286bea4c9b48eccb53fc6ec14a4
jtomli03 <jotom1000@gmail.com>
2015-09-26 09:44:02 +0100

    new gitignore

---
+++ src/main/java/com/sky/bean/ProductImpl.java
@@ -0,0 +1,49 @@
+package com.sky.bean;
+
+import java.util.List;
+
+/**
+ * Created by Jo on 25/09/2015.
+ */
+public class ProductImpl implements Product {
+
+    @Override
+    public Double getPrice() {
+        return null;
+    }
+
+    @Override
+    public void setPrice(Double price) {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public void setDescription(String description) {
+
+    }
+
+    @Override
+    public void addProductSupplier(ProductSupplier supplier) {
+
+    }
+
+    @Override
+    public List<ProductSupplier> getProductSuppliers() {
+        return null;
+    }
+}
